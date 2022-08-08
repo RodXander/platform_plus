48969d7b1d1f77784fa69b33911f885bcfde9278
rodxander <oasl.rodxander@gmail.com>
2022-08-08 11:42:08 -0600

    Updated the plugin to an export approach for better access to its API.

--- lib/platform_plus.dart
+++ lib/platform_plus.dart
@@ -1,20 +1,2 @@
-import 'dart:io';
-
-import 'package:flutter/services.dart';
-
-class PlatformPlus {
-  static const methodChannel = MethodChannel('com.rodxander.platform_plus');
-
-  PlatformPlus._();
-
-  static Future<PlatformPlus> getInstance() async {
-    if (Platform.isAndroid) {
-      _platformPlus.androidSdkNumber = await methodChannel.invokeMethod('getAndroidSdkNumber');
-    }
-    return _platformPlus;
-  }
-
-  static final PlatformPlus _platformPlus = PlatformPlus._();
-
-  late int androidSdkNumber;
-}
+export 'src/android_version_codes.dart';
+export 'src/platform_plus.dart';
